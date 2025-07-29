7c9cd7325b2503fce16d5e9a2ba1b4a0fbcc1064
xuanthedev <NXT1008@users.noreply.github.com>
2025-07-29 10:13:15 +0700

    Build Production

--- src/util/apiEndpoint.js
+++ src/util/apiEndpoint.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'https://money-management-backend-b5ih.onrender.com/api/v1.0'
+export const BASE_URL = 'https://money-manager-api-ijz7.onrender.com/api/v1.0'
 
-export const BASE_URL = 'http://localhost:8080/api/v1.0'
+// export const BASE_URL = 'http://localhost:8080/api/v1.0'
 
 export const API_ENDPOINTS = {
   LOGIN: '/login',
